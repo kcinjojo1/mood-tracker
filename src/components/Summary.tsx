ae4a17aed39f192d164ff41d289b278a65860351
kcinjojo1 <58200261+kcinjojo1@users.noreply.github.com>
2025-09-20 15:49:40 -0400

    Create Summary.tsx

---
+++ src/components/Summary.tsx
@@ -0,0 +1,27 @@
+type Props = {
+  red: number
+  green: number
+  title: string
+}
+
+export default function Summary({ red, green, title }: Props){
+  const total = red + green
+  const pctGreen = total ? Math.round((green/total)*100) : 0
+
+  return (
+    <div className="summary">
+      <div className="summaryItem">
+        <div className="small">{title} Greens</div>
+        <div className="kpi">{green}</div>
+      </div>
+      <div className="summaryItem">
+        <div className="small">{title} Reds</div>
+        <div className="kpi">{red}</div>
+      </div>
+      <div className="summaryItem">
+        <div className="small">Green %</div>
+        <div className="kpi">{pctGreen}%</div>
+      </div>
+    </div>
+  )
+}
